a6edcef3f4843357d0f42889829aa863ed3508db
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-03-05 14:54:07 -0500

    auth fully complete, added redirect to proper page

--- src/auth.js
+++ src/auth.js
@@ -101,5 +101,41 @@ export class Auth {
         }
     }
 
-    
+    async redirect() {
+        const headers = {
+            'Host': 'cssa.cunyfirst.cuny.edu',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'dnt': '1',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Site': 'same-site',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-US,en;q=0.9',
+        };
+
+        try {
+            let baseURL = 'https://cssa.cunyfirst.cuny.edu';
+            let response = await this.client.get(`${baseURL}${this.redirectURL}`, { headers });
+
+            //may need to switch to name instead of user
+            if (response.body.includes(config.username)) {
+                console.log('Successfully logged in');
+                return true;
+            } else {
+                console.log('Login failed. Check credentials.');
+                return false;
+            }
+        } catch (e) {
+            console.log('ERROR:', e.message);
+            return false;
+        }
+    }
 }
