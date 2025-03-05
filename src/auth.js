d0cf09c657d99e800ab7c2d3872934c07af589eb
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-03-05 14:53:32 -0500

    login almost complete

--- src/auth.js
+++ src/auth.js
@@ -52,5 +52,54 @@ export class Auth {
             console.error('Error Message:', e.message);
         }
     }
+
+    async login() {
+        const loginURL = 'https://ssologin.cuny.edu/oam/server/auth_cred_submit';
+
+        const headers = {
+            'Host': 'ssologin.cuny.edu',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'Origin': 'https://ssologin.cuny.edu',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'dnt': '1',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Referer': 'https://ssologin.cuny.edu/cuny.html',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-US,en;q=0.9',
+        };
+
+        const payload = new URLSearchParams({
+            usernameH: config.email,
+            username: config.username,
+            password: config.password,
+            submit: '',
+        }).toString();
+
+        console.log('Attempting login...');
+        try {
+            let response = await this.client.post(loginURL, {
+                headers,
+                body: payload,
+                followRedirect: false,
+            });
+
+            const redirectURL = response.headers['location'];
+            const urlObject = new URL(redirectURL);
+            this.redirectURL = urlObject.pathname + urlObject.search;
+        } catch (e) {
+            console.error('Error Message:', e.message);
+        }
+    }
+
     
 }
